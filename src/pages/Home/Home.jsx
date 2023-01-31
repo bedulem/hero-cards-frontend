ed02103ea7220fbb80e53eed1e3fe794b6b3eb2e
bedulem <santochibelen@gmail.com>
2023-01-31 17:13:12 -0300

    add home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="bg-black/50 h-full w-full">
+      <div className="flex justify-center h-full items-center ">
+        <img
+          className="w-80"
+          src="./logos/HeroRealmsLogo.png"
+          alt="logo Hero Realms"
+        />
+      </div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+    </div>
+  );
+};
+
+export default Home;
